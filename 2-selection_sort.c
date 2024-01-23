fc6d8ddd126f1ce4ab88eae232c133c21e21ddda
habiba zguaid <zguaidhabiba@gmail.com>
2024-01-23 11:30:27 -0800

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+/**
+ *selection_sort - sorts an array of integers in ascending
+ *order using the selection sort algorithm
+ *@array: the array to sort
+ *@size: the size of the array
+ * 
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, k;
+	int temp;
+
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = size - 1, k = i + 1; j > i; j--)
+		{
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
+		}
+		if (array[i] > array[k])
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
+	}
+}
